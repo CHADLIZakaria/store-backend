bff050fc9429d93d053e7d3e320989cad052e697
CHADLI <zakaria.chadli@capgemini.com>
2022-07-21 10:55:27 +0100

    change to new database

--- src/main/java/com/zchadli/myrestauservice/MyRestauServiceApplication.java
+++ src/main/java/com/zchadli/myrestauservice/MyRestauServiceApplication.java
@@ -28,6 +28,6 @@ public class MyRestauServiceApplication implements CommandLineRunner {
     @Bean
     public BCryptPasswordEncoder  passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
+	}
 
 }
